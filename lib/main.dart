796c1ac92970f8b224284668b048d70168bfe97a
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-14 16:07:53 +0000

    feat: implement MyCalculator

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+class MyCalculator extends StatefulWidget {
+  const MyCalculator({super.key});
+
+  @override
+  State<MyCalculator> createState() => _MyCalculatorState();
+}
+
+class _MyCalculatorState extends State<MyCalculator> {
  
   @override
   Widget build(BuildContext context) {
