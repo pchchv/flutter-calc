1b8bcdc1e155d231a01282a5072473d7bd241350
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-14 16:24:22 +0000

    methods to enable functionality for each button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,7 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,12 +36,42 @@ class _MyCalculatorState extends State<MyCalculator> {
   
   // Stores the calculation result
   var output = '';
- 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-      ),
-    );
+
+  void _onButtonClicked(String value) {
+    setState(() {
+      if (value == "AC") {
+        input = '';
+        output = '';
+      } else if (value == "A") {
+        if (input.isNotEmpty) {
+          input = input.substring(0, input.length - 1);
+        }
+      } else if (value == "=") {
+        if (input.isNotEmpty) {
+          try {
+            final parser = GrammarParser();
+            final expression = parser.parse(input);
+            final evaluator = RealEvaluator();
+            final num finalValue = evaluator.evaluate(expression);
+            output = finalValue.toString();
+            if (output.endsWith(".0")) {
+              output = output.substring(0, output.length - 2);
+            }
+          } catch (e) {
+            output = "Error";
+          }
+        }
+      } else {
+        input += value;
+      }
+    });
   }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Column(
+    ),
+  );
+}
 }
