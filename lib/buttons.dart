6a7d259b26f6674a11afc0a3fa4457be38c27c96
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-19 12:48:24 +0000

    feat: implement styling and layout for MyButton

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -18,6 +18,25 @@ class MyButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: buttonTapped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: textColor, 
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
