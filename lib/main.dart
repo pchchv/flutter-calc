0eec70dcf7bd733d69df58f0f32d7271837d4447
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-19 12:35:40 +0000

    feat: implement HomePage with basic Scaffold and layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(children: []),
     );
   }
 }
